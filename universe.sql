8ccf37f968b45ecb398968f5b4170f63be05c16a
sanjayvarma56 <dssvarma56@gmail.com>
2026-05-27 09:37:53 +0530

    Update comment formatting in universe.sql

--- universe.sql
+++ universe.sql
@@ -1,7 +1,7 @@
 --
 -- PostgreSQL database dump
 --
-
+--
 -- Dumped from database version 12.22 (Ubuntu 12.22-0ubuntu0.20.04.4)
 -- Dumped by pg_dump version 12.22 (Ubuntu 12.22-0ubuntu0.20.04.4)
 
