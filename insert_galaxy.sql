6072325aaf2907b7125fee796d31ca3f2f42d6cb
sanjayvarma56 <dssvarma56@gmail.com>
2026-07-06 09:27:00 +0530

    Inserting galaxy data

---
+++ insert_galaxy.sql
@@ -0,0 +1,22 @@
+--
+-- Data for Name: galaxy
+-- Type: TABLE DATA
+--
+
+INSERT INTO public.galaxy VALUES
+(1, 'Milky Way', NULL, true, 13600, NULL);
+
+INSERT INTO public.galaxy VALUES
+(2, 'Andromeda', NULL, false, 10000, NULL);
+
+INSERT INTO public.galaxy VALUES
+(3, 'Triangulum', NULL, false, 12000, NULL);
+
+INSERT INTO public.galaxy VALUES
+(4, 'Sombrero', NULL, false, 13000, NULL);
+
+INSERT INTO public.galaxy VALUES
+(5, 'Centaurus', NULL, false, 11000, NULL);
+
+INSERT INTO public.galaxy VALUES
+(6, 'Whirlpool', NULL, false, 10500, NULL);
