9edb2af15f9f5bdd90c880e1bf71573d47daf918
sanjayvarma56 <dssvarma56@gmail.com>
2026-07-07 07:14:05 +0530

    Inserting Planet data

---
+++ insert_planet.sql
@@ -0,0 +1,40 @@
+--
+-- Data for Name: planet
+-- Type: TABLE DATA
+--
+
+INSERT INTO public.planet VALUES
+(13, 'Earth', true, true, NULL, 1);
+
+INSERT INTO public.planet VALUES
+(14, 'Mars', false, true, NULL, 1);
+
+INSERT INTO public.planet VALUES
+(15, 'Venus', false, true, NULL, 1);
+
+INSERT INTO public.planet VALUES
+(16, 'Jupiter', false, true, NULL, 1);
+
+INSERT INTO public.planet VALUES
+(17, 'Saturn', false, true, NULL, 1);
+
+INSERT INTO public.planet VALUES
+(18, 'Mercury', false, true, NULL, 1);
+
+INSERT INTO public.planet VALUES
+(19, 'Proxima b', false, true, NULL, 3);
+
+INSERT INTO public.planet VALUES
+(20, 'Osiris', false, true, NULL, 3);
+
+INSERT INTO public.planet VALUES
+(21, 'Kepler-22b', false, true, NULL, 4);
+
+INSERT INTO public.planet VALUES
+(22, 'Gliese 581g', false, true, NULL, 4);
+
+INSERT INTO public.planet VALUES
+(23, 'Tatooine', false, true, NULL, 5);
+
+INSERT INTO public.planet VALUES
+(24, 'Hoth', false, true, NULL, 5);
