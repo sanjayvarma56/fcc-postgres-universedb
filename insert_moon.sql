7f741b78fdbf74173b86d67787013bdfe601d5be
sanjayvarma56 <dssvarma56@gmail.com>
2026-07-07 07:14:40 +0530

    Inserting Moon Data

---
+++ insert_moon.sql
@@ -0,0 +1,64 @@
+--
+-- Data for Name: moon
+-- Type: TABLE DATA
+--
+
+INSERT INTO public.moon VALUES
+(41, 'The Moon', true, 1737, 13);
+
+INSERT INTO public.moon VALUES
+(42, 'Phobos', false, 11, 14);
+
+INSERT INTO public.moon VALUES
+(43, 'Deimos', false, 6, 14);
+
+INSERT INTO public.moon VALUES
+(44, 'Io', true, 1821, 16);
+
+INSERT INTO public.moon VALUES
+(45, 'Europa', true, 1560, 16);
+
+INSERT INTO public.moon VALUES
+(46, 'Ganymede', true, 2634, 16);
+
+INSERT INTO public.moon VALUES
+(47, 'Callisto', true, 2410, 16);
+
+INSERT INTO public.moon VALUES
+(48, 'Titan', true, 2574, 17);
+
+INSERT INTO public.moon VALUES
+(49, 'Enceladus', true, 252, 17);
+
+INSERT INTO public.moon VALUES
+(50, 'Mimas', true, 198, 17);
+
+INSERT INTO public.moon VALUES
+(51, 'Dione', true, 561, 17);
+
+INSERT INTO public.moon VALUES
+(52, 'Rhea', true, 763, 17);
+
+INSERT INTO public.moon VALUES
+(53, 'Iapetus', true, 734, 17);
+
+INSERT INTO public.moon VALUES
+(54, 'Tethys', true, 531, 17);
+
+INSERT INTO public.moon VALUES
+(55, 'Miranda', true, 235, 18);
+
+INSERT INTO public.moon VALUES
+(56, 'Ariel', true, 578, 18);
+
+INSERT INTO public.moon VALUES
+(57, 'Umbriel', true, 584, 18);
+
+INSERT INTO public.moon VALUES
+(58, 'Titania', true, 788, 18);
+
+INSERT INTO public.moon VALUES
+(59, 'Oberon', true, 761, 18);
+
+INSERT INTO public.moon VALUES
+(60, 'Triton', true, 1353, 18);
