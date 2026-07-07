3011c2a7ac87ee11d0cf4d9fab13dc9d8100ae55
sanjayvarma56 <dssvarma56@gmail.com>
2026-07-07 07:13:27 +0530

    Inserting star data

---
+++ insert_star.sql
@@ -0,0 +1,22 @@
+--
+-- Data for Name: star
+-- Type: TABLE DATA
+--
+
+INSERT INTO public.star VALUES
+(1, 'Sun', true, NULL, 1, 1);
+
+INSERT INTO public.star VALUES
+(2, 'Sirius', true, NULL, 2, 1);
+
+INSERT INTO public.star VALUES
+(3, 'Alpha Centauri', true, NULL, 1, 1);
+
+INSERT INTO public.star VALUES
+(4, 'Betelgeuse', true, NULL, 15, 2);
+
+INSERT INTO public.star VALUES
+(5, 'Rigel', true, NULL, 20, 2);
+
+INSERT INTO public.star VALUES
+(6, 'Vega', true, NULL, 2, 3);
