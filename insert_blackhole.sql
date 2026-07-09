86460ed21e9052f3cc51bb140681ead46ea5bf69
sanjayvarma56 <dssvarma56@gmail.com>
2026-07-09 21:28:34 +0530

    Inserting blackhole data

---
+++ insert_blackhole.sql
@@ -0,0 +1,13 @@
+--
+-- Data for Name: blackhole
+-- Type: TABLE DATA
+--
+
+INSERT INTO public.blackhole VALUES
+(1, 'Sagittarius A*', true, 4000000);
+
+INSERT INTO public.blackhole VALUES
+(2, 'M87*', true, 6000000);
+
+INSERT INTO public.blackhole VALUES
+(3, 'Gargantua', false, 10000000);
